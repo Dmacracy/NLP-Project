5252fe6ae8391805ac9ea6024879c843d1f7cc9f
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2020-04-22 20:04:39 -0700

    Added function comments.

--- src/NarrativeQAData.py
+++ src/NarrativeQAData.py
@@ -9,6 +9,10 @@ from cdqa.pipeline import QAPipeline
 from cdqa.utils.download import download_model
 
 def split_train_test_val(qcsv):
+    '''
+    Splits narrative QA dataset 
+    into three dataframes: train, validation, and test.
+    '''
     qs = pd.read_csv(qcsv)
     qsTrain = qs.loc[qs['set'] == 'train']
     qsVal = qs.loc[qs['set'] == 'valid']
@@ -17,7 +21,13 @@ def split_train_test_val(qcsv):
 
 
 def ask_doc_qs(docdf, model, qs):
-    print(docdf)
+    '''
+    Given docdf, a pandas dataframe representing a document 
+    or group of documents with columns 'title' and 'paragraphs', 
+    a joblib model, and a list of questions, this function
+    makes cdqa queries for each question and prints the predicted 
+    answers.
+    '''
     cdqa_pipeline = QAPipeline(reader=model, min_df=1, max_df=5.0)
     cdqa_pipeline.fit_retriever(df=docdf)
     for q in qs:
@@ -30,6 +40,11 @@ def ask_doc_qs(docdf, model, qs):
 
 
 def pred(qdf, storiesDir, model):
+    '''
+    Given a narrativeqa dataframe, a directory where the narrative 
+    stories are located, and a joblib model, this function asks the questions 
+    corresponding to each document using teh ask_doc_qs function above. 
+    '''
     docIds = qdf['document_id'].unique()
     for docId in docIds[:1]:
         docEntries = qdf.loc[qdf['document_id'] == docId]
