1e8d50e3c64959f172a52bec6b2a7b82e11308fc
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2020-04-23 20:04:18 -0700

    removed DrQA interactive-like calls and just started converting to the file format expected by DrQA's predict pipeline.

--- src/NarrativeQAData.py
+++ src/NarrativeQAData.py
@@ -12,6 +12,7 @@ import logging
 from termcolor import colored
 from drqa import pipeline
 from drqa.retriever import utils
+import drqa.tokenizers
 
 from cdqa.utils.converters import pdf_converter
 from cdqa.utils.filters import filter_paragraphs
@@ -48,31 +49,6 @@ def ask_doc_qs_cdqa(docdf, model, qs):
         print('paragraph: {}'.format(prediction[2]))
         print("\n\n")
         
-
-def Drqa_process(question, candidates=None, top_n=1, n_docs=5):
-    '''Make a DRQA question'''
-    predictions = DrQA.process(
-        question, candidates, top_n, n_docs, return_context=True
-    )
-    table = prettytable.PrettyTable(
-        ['Rank', 'Answer', 'Doc', 'Answer Score', 'Doc Score']
-    )
-    for i, p in enumerate(predictions, 1):
-        table.add_row([i, p['span'], p['doc_id'],
-                       '%.5g' % p['span_score'],
-                       '%.5g' % p['doc_score']])
-    print('Top Predictions:')
-    print(table)
-    print('\nContexts:')
-    for p in predictions:
-        text = p['context']['text']
-        start = p['context']['start']
-        end = p['context']['end']
-        output = (text[:start] +
-                  colored(text[start: end], 'green', attrs=['bold']) +
-                  text[end:])
-        print('[ Doc = %s ]' % p['doc_id'])
-        print(output + '\n')
         
 
 def pred(qdf, storiesDir, model):
@@ -95,72 +71,28 @@ def pred(qdf, storiesDir, model):
                 Drqa_process(q)
 
 
-
+def narrative_df_to_squadlike(df, outName):
+    '''
+    Convert the dataframe representing the set of NarrativeQA
+    questions into the SQUaD-like txt file format expected by DrQA.
+    '''
+    df['answer'] = df[["answer1","answer2"]].values.tolist()
+    Json = df[["question", "answer"]].to_json(orient="records", lines=True)
+    with open(outName, 'w') as outfile:
+        outfile.write(Json)
+    
         
 
-
 if __name__ == "__main__":
-    drqa.tokenizers.set_default('corenlp_classpath', './data/corenlp')
-    tok = drqa.tokenizers.CoreNLPTokenizer()
-    
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
-    console = logging.StreamHandler()
-    console.setFormatter(fmt)
-    logger.addHandler(console)
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--reader-model', type=str, default=None,
-                        help='Path to trained Document Reader model')
-    parser.add_argument('--retriever-model', type=str, default=None,
-                        help='Path to Document Retriever model (tfidf)')
-    parser.add_argument('--doc-db', type=str, default=None,
-                        help='Path to Document DB')
-    parser.add_argument('--tokenizer', type=str, default=None,
-                        help=("String option specifying tokenizer type to "
-                              "use (e.g. 'corenlp')"))
-    parser.add_argument('--candidate-file', type=str, default=None,
-                        help=("List of candidates to restrict predictions to, "
-                              "one candidate per line"))
-    parser.add_argument('--no-cuda', action='store_true',
-                        help="Use CPU only")
-    parser.add_argument('--gpu', type=int, default=-1,
-                        help="Specify GPU device id to use")
-    args = parser.parse_args()
-    
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
-    if args.cuda:
-        torch.cuda.set_device(args.gpu)
-        logger.info('CUDA enabled (GPU %d)' % args.gpu)
-    else:
-        logger.info('Running on CPU only.')
-
-    if args.candidate_file:
-        logger.info('Loading candidates from %s' % args.candidate_file)
-        candidates = set()
-        with open(args.candidate_file) as f:
-            for line in f:
-                line = utils.normalize(line.strip()).lower()
-                candidates.add(line)
-        logger.info('Loaded %d candidates.' % len(candidates))
-    else:
-        candidates = None
-
-    logger.info('Initializing DRQA pipeline...')
-    DrQA = pipeline.DrQA(
-    cuda=args.cuda,
-    fixed_candidates=candidates,
-    reader_model=args.reader_model,
-    ranker_config={'options': {'tfidf_path': args.retriever_model}},
-    db_config={'options': {'db_path': args.doc_db}},
-    tokenizer=args.tokenizer
-    )
-
     qcsv = './data/narrativeqa/qaps.csv'
     storiesDir = './data/narrativeqa/stories'
     model = './models/bert_qa.joblib'
     qsTrain, qsVal, qsTest = split_train_test_val(qcsv)
+    
+    narrative_df_to_squadlike(qsTrain, './data/narrativeqa/train.txt')
+    narrative_df_to_squadlike(qsVal, './data/narrativeqa/val.txt')
+    narrative_df_to_squadlike(qsTest, './data/narrativeqa/test.txt')
+    
     pred(qsTrain, storiesDir, model)
 
 
