734320230c182220d104ebf436697690d7be5965
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2020-04-27 19:34:32 -0700

    Created scripts for training both squad datasets

--- src/run_squad_2.sh
+++ src/run_squad_2.sh
@@ -5,11 +5,12 @@ python3 run_squad.py \
   --model_name_or_path distilbert-base-uncased-distilled-squad \
   --do_train \
   --do_eval \
-  --train_file $SQUAD_DIR/SQuAD-v1.1-train.json \
-  --predict_file $SQUAD_DIR/SQuAD-v1.1-dev.json \
+  --train_file $SQUAD_DIR/SQuAD-v2.0-train.json \
+  --predict_file $SQUAD_DIR/SQuAD-v2.0-dev.json \
   --per_gpu_train_batch_size 1 \
   --learning_rate 3e-5 \
   --num_train_epochs 2.0 \
   --max_seq_length 384 \
   --doc_stride 128 \
-  --output_dir ../data/SQuAD_out/
+  --version_2_with_negative \
+  --output_dir ../data/SQuAD_2_out/
